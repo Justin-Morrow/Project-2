d528268f72f614926bc300052130649f954e7884
dhuw <hazlewood.dyl@gmail.com>
2022-07-16 13:43:33 -0700

    made create account btn functional
    made the "create account" button on the home page take the user to the sign up page

--- public/js/lobby.js
+++ public/js/lobby.js
@@ -1,1 +1,6 @@
 // this file is for the sign out buttons func
+
+//find where signed in status is stored
+
+//when sign out button is clicked in navbar
+//then signed in status = false and go to home menu
