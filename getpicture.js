5cca76708d33aa115e17ad0fc5156fe258765f51
Christina Hong <102135325+c1008h@users.noreply.github.com>
2022-07-16 22:02:43 -0700

    Update getpicture.js
    I am not sure if it will work correctly with the userinput but all the console logs seem to log what i wanted it to. 
    
    User input = true then it will go on and randomize a photo from that breed. But that would be returned as a link to the photo so have to save that link inside an image tag. 
    
    If what userinput was not valid then it will prompt that it was invalid.

--- getpicture.js
+++ getpicture.js
@@ -1,22 +1,38 @@
-const fetch = require('node-fetch')
+const fetch = require('node-fetch');
 
-const dogBreed = 'chihuahua';
-const dogImage = 'https://dog.ceo/api/breed/' + dogBreed + '/images'
+//const url = `https://dog.ceo/api/breed/${dogBreed}/images`
+
+// This was to test if breed would show up
+// let breed = 'terrier wheaten'
 
 
-console.log(dogImage)
-
-
-fetch(dogImage)
-.then(res => res.json())
+// Getting a list of all breeds from API
+fetch('https://dog.ceo/api/breeds/list/all')
+.then (res => res.json())
 .then(data => {
-    //console.log(data)
+    console.log(data.message)
+    console.log(data.message.spaniel.includes('irish'))
+    console.log('data.message', Object.keys(data.message).includes('mix'))
 
-    let x = Math.floor((Math.random() * data.message.length));
-    //console.log(x)
-    console.log(data.message[x])
+    const userinput = `${breed}`
 
+    if(Object.keys(data.message).includes(userinput)) {
+        fetch(`https://dog.ceo/api/breed/${breed}/images`)
+        .then(res => res.json())
+        .then(data => {
+            // console.log(data)
+            let x = Math.floor((Math.random() * data.message.length));
+            console.log(x)
+            console.log(data.message[x])
+            
+            const profilePicture = document.createElement('img')
+            profilePicture = data.message[x]
+        })
+        .catch(err => console.log(err))
+    } else {
+        prompt(`${breed} is invalid. Please enter a valid breed.`);
+    }
 })
-
 .catch(err => console.log(err))
+
 
