8267e9695f8f0a2f203ae868212a0ba8a2b2b8c3
dave <davidpascual200@gmail.com>
2022-07-17 22:09:50 -0700

    fixed port

--- server.js
+++ server.js
@@ -8,27 +8,29 @@ const helpers = require('./utils/helpers')
 const sequelize = require('./config/connection');
 
 //store user cookies in database
-const sequelizeStore = require('connect-session-sequelize')(session.Store)
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express(); //what does this do ?
-const PORT = process.env.PORT || 3306;
+const PORT = process.env.PORT || 3001;
 
 const hbs = exphbs.create({ helpers }); //what does this do?
 
 //config for sessions on express server
 const sess = {
-    secret: "Super secret secret",
+    secret: 'Super secret secret',
     cookie: {},
     resave: false,
     saveUninitialized: true,
-    store: new sequelizeStore({
-        db: sequelize
+    store: new SequelizeStore({
+      db: sequelize
     })
-};
+  };
+  
 
 //add middleware
 app.use(session(sess));
 
+// Inform Express.js on which template engine to use
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
@@ -39,5 +41,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
-    app.listen(PORT, () => console.log('Now listening'))
-})
+  app.listen(PORT, () => console.log('Now listening'));
+});
