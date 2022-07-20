f4182d4b2e76f856e5b57b7f333557269533b709
Justin Morrow <morrowjustin19@gmail.com>
2022-07-19 20:39:15 -0700

    adding API controllers and routes

---
+++ controllers/api/lobbyController.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const router = express.Router;
+const { User, UserMatch, Lobby } = require("../../models");
+
+// Basic Get Requests 
+// To Get All Lobbies
+
+// To Get One Lobby
+
+// Create new Lobby API route that includes the logged in user 
+
+// Update Lobby to Include a Second User 
+
+// Delete Lobby
+
+
--- controllers/api/userController.js
+++ controllers/api/userController.js
@@ -4,4 +4,131 @@ const router = express.Router();
 // connecting with socket io-server
 const io = require("../../server");
 const { User, UserMatches, Lobby } = require('../../models');
+const bcrypt = require("bcrypt");
+// const { beforeDestroy } = require('../../models/Dog');
+// const { route } = require('.');
+// const { default: ModelManager } = require('sequelize/types/model-manager');
+// const { Model } = require('sequelize/types');
+
+// API routes
+// Get landing page
+router.get("/", (req, res) =>{
+User.findAll().then(UserData =>{
+    res.json({UserData});
+}).catch(err =>{
+    console.log(err);
+    res.status(500).json(err);
+})
+});
+
+// Get Logout page
+router.get("/logout", (req, res) =>{
+    req.session.destroy(()=> {
+        res.json({msg: "session is closed"});
+        res.redirect("/");
+    })
+});
+
+// Get by ID 
+router.get("/:id", (req, res) =>{
+    User.findByPk(req.params.id).then(UserData =>{
+        if(UserData){
+            res.json({UserData})
+        } else {
+            res.status(404).json({err: "No user found"})
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    })
+});
+
+// Create a User API route
+route.post("/", (req,res)=>{
+    User.create({
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    }).then(newUser => {
+        req.session.user = {
+            username: newUser.username,
+            email: newUser.email,
+            id: newUser.id
+        }
+        res.json({newUser});
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({message: ""})
+    })
+})
+
+// login form route 
+router.post("/login", (req, res) =>{
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(foundUser =>{
+        if(!foundUser){
+            req.session.destroy();
+            res.status(401).json({message: "Incorrect email or password"})
+        } else {
+            if (bcrypt.compareSync(req.body.password, foundUser.password)){
+                req.session.user = {
+                    username: foundUser.username,
+                    email: foundUser.email,
+                    id: foundUser.id
+                }
+                res.json({foundUser})
+            } else {
+                req.session.destroy();
+                res.status(401).json({message: "Incorrect email or Password"})
+            }
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+// Update user route 
+router.put("/:id", (req, res) =>{
+    User.update({
+        username: req.body.username,
+        email: req.body.email,
+        name: req.body.name,
+        description: req.body.description,
+        breed: req.body.breed,
+        age: req.body.age,
+        gender: req.body.gender,
+        location: req.body.location
+    }, {
+        where: {
+            id: req.params.id
+        }
+    }).then(updatedData =>{
+        if(updatedData[0]) {
+            res.json({updatedData});
+        } else {
+            res.status(404).json({err: "User not found"});
+        }
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({err});
+    });
+});
+
+// Add a match route
+router.post("/lobby", (req, res) =>{
+    // add a friend - takes in sesion id and puts as user 1
+    // takes in input user id and puts as user 2 - email 
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    });
+});
+
+module.exports = router;
+
 
