7fd9ff488ab03387157204b51607e0858b8cf050
Justin Morrow <morrowjustin19@gmail.com>
2022-07-19 22:10:23 -0700

    reviewing files ank making sure they are back in shape before moving out of controllers

--- controllers/index.js
+++ controllers/index.js
@@ -2,27 +2,25 @@ const express = require('express');
 const router = require('express').Router(); 
 
 // // front end handlebars work
-// const frontEndRoutes = require('./frontEndRoutes');
-// router.use("/", frontEndRoutes);
+const frontEndRoutes = require('./frontEndRoute');
+router.use("/", frontEndRoutes);
 
 // back end sql work 
 const apiRoutes = require('./api');
-const sessionRoutes = require('./sessionRoutes');
-
 router.use('/api', apiRoutes);
-router.use('/', sessionRoutes)
-
-const { restore } = require('../models/Dog');
-
 
 // log sessions
 router.get("/sessions", (req,res)=>{
     res.json(req.session)
 })
 
+module.exports = router;
+
 // I commented out because it is not needed :)
 // router.use((req,res) => {
 //     res.send("<h1>Wrong Route!</h1>") //added in for check
 // })
+// router.use('/', sessionRoutes)
+// const { restore } = require('../models/Dog');
+// const sessionRoutes = require('./sessionRoutes');
 
-module.exports = router;
